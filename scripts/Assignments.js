6fe7172bc31b48be49b69bcf4d10dd5dd21859f9
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-02 11:59:34 -0500

    finished debug

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -9,7 +9,7 @@ const walkers = getWalkers()
 const findWalker = (pet, allWalker) => {
     let petWalker = null
 
-    for (const walker of allWalkers) {
+    for (const walker of allWalker) {
         if (walker.id === pet.walkerId) {
             petWalker = walker
         }
@@ -20,11 +20,11 @@ const findWalker = (pet, allWalker) => {
 
 export const Assignments = () => {
     let assignmentHTML = ""
-    assignmentHTML = "<ul>"
+    assignmentHTML += "<ul>"
 
     for (const currentPet of pets) {
-        const currentPetWalker = findPetWalker(currentPet, walkers)
-        assignmentHTML = `
+        const currentPetWalker = findWalker(currentPet, walkers)
+        assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
                 ${currentPetWalker.name} in ${currentPetWalker.city}
--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,16 +1,17 @@
-import { database } from "./database.js"
+import { getWalkers } from "./database.js"
 
-const walkers = database.walkers
+const walkers = getWalkers()
 
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.fullName}</li>`
+        walkerHTML += `<li>${walker.name}</li>`
     }
 
     walkerHTML += "</ul>"
+    return walkerHTML
 
 }
 
