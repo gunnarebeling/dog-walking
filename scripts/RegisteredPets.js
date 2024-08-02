b3a01228db34ef37f44f2a6dd53d03929c1b8410
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-02 15:14:22 -0500

    added click event for walkers and pets

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,31 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
 const pets = getPets()
+
+
+document.addEventListener("click",
+    (clickevent) =>{
+      const whatWasClicked = clickevent.target
+      const walkerid = whatWasClicked.dataset.walkerforeignkey
+      const walkers = getWalkers()
+      for (const walker of walkers) {
+        if(walker.id === parseInt(walkerid)){
+            window.alert(`this pet is being walked by ${walker.name}`)
+        }
+        
+      }
+
+      
+      
+    }
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li data-type="pet" data-walkerforeignkey="${pet.walkerId}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
