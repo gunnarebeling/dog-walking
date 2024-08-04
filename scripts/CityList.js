d3629a99fa5edbb63a792a7323cdde5e407b2e4b
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-04 12:43:50 -0500

    added new cities obj to database

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -2,12 +2,22 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener("click", 
+    (clickedItem) =>{
+        const itemClicked = clickedItem.target
+        if (itemClicked.dataset.type === "city") {
+            
+            window.alert(`${itemClicked.dataset.walkername} is servicing this city`)
+        }
+    }
+)
+
 
 export const CityList = () => {
     let citiesHTML = "<ol>"
 
     for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+        citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`
     }
 
     citiesHTML += "</ol>"
